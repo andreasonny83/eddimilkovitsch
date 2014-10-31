55097dddd228614deaa554cc11030e53135352ce
Sonny, Andrea <andreasonny83@gmail.com>
2014-10-31 13:36:13 +0000

    Interactive menu

---
+++ js/eddimikovitsch.js
@@ -0,0 +1,84 @@
+// Global variables
+var menu_items,
+	article_items = [],
+	articles;
+
+// highlight the menu item accoding the current Y position
+function highlightCurrentItem( posY ) {
+
+	jQuery.each( article_items, function( i, val ) {
+		// console.log( val.position );
+		if ( ( posY + 10 ) > val.position ) {
+			$( "#main_menu li" ).removeClass( "highlight" );
+			$( "." + val.name ).addClass( "highlight" );
+		}
+	});
+
+}
+
+$( document ).ready( function() {
+
+	// store menu items inside menu_items
+	menu_items 	= $( "#main_menu li" );
+	articles	= $( "article" );
+
+	// get the Articles position and store them inside an object array
+	// set 'xxx' to 'home' if you want to highlight the Home button
+	article_items.push({ name: 'xxx', position: 0 });
+
+	articles.each( function( item ) {
+
+		var name, item_position;
+
+		name 			= $(this).attr('id');
+		item_position 	= $(this).position().top;
+
+		// store the article positions inside an object array
+		article_items.push({ name: name, position: item_position });
+
+	});
+
+	/**
+	Navigation menu
+	**/
+		menu_items.click( function( e ) {
+
+			var link = "#" + $(this).attr('class');
+			link = link.replace(/\s(.*)/,'');
+
+			console.log( link );
+			$('html, body').animate({scrollTop : $( link ).position().top },500);
+
+			return false;
+
+		});
+
+	// Highlight the correct menu item
+	var posY = $( window ).scrollTop();
+	highlightCurrentItem( posY );
+
+});
+
+// On page scroll...
+$( window ).scroll( function() {
+	
+	var posY = $( window ).scrollTop();
+
+	// highlight the menu item according to the current Y position
+	highlightCurrentItem( posY );
+
+	// Show the fixed menu
+	if ( posY > 420 ) {
+		if ( !$( "#navbar" ).hasClass( "fixed" ) ) {
+			$( "#navbar" ).hide().addClass( "fixed" ).fadeIn( 500 );
+		}
+	}
+	// Remove the fixed menu
+	else {
+		if ( $( "#navbar" ).hasClass( "fixed" ) ) {
+			$( "#navbar" ).removeClass( "fixed" );
+		}
+	}
+	
+});
+
