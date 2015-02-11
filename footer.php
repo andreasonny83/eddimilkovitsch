c001562e7633b87975c748a062a4c1101daccc97
andreasonny83 <andreasonny83@gmail.com>
2015-02-11 21:28:35 +0000

    Social media links updated

--- footer.php
+++ footer.php
@@ -2,10 +2,10 @@
 			<div class="menu row">
 				<ul class="social">
 					<li class="description"><?php echo $footer[$lang][0] ?></li>
-					<li class="social facebook"><a href="http://www.facebook.com/" class="btn-social btn-outline" target="_blank"><i class="fa fa-facebook"></i></a></li>
-					<li class="social gplus"><a href="http://plus.google.com/" class="btn-social btn-outline" target="_blank"><i class="fa fa-fw fa-google-plus"></i></a></li>
-					<li class="social twitter"><a href="http://twitter.com/" class="btn-social btn-outline" target="_blank"><i class="fa fa-fw fa-twitter"></i></a></li>
-					<li class="social linkedin"><a href="http://linkedin.com/" class="btn-social btn-outline" target="_blank"><i class="fa fa-fw fa-linkedin"></i></a></li>
+					<li class="social facebook"><a href="https://www.facebook.com/eddi.milkovitsch" class="btn-social btn-outline" target="_blank"><i class="fa fa-facebook"></i></a></li>
+					<!-- <li class="social gplus"><a href="http://plus.google.com/" class="btn-social btn-outline" target="_blank"><i class="fa fa-fw fa-google-plus"></i></a></li> -->
+					<li class="social twitter"><a href="https://twitter.com/ready_eddi" class="btn-social btn-outline" target="_blank"><i class="fa fa-fw fa-twitter"></i></a></li>
+					<!-- <li class="social linkedin"><a href="http://linkedin.com/" class="btn-social btn-outline" target="_blank"><i class="fa fa-fw fa-linkedin"></i></a></li> -->
 				</ul>
 			</div>
 			<div class="copyright row">
